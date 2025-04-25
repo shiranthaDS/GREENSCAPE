9adf3f5cb5f2deefcb58a18a0d53bbebc7ca56e0
Shalon Fernando <164470858+3hal0n@users.noreply.github.com>
2025-04-25 21:41:48 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,35 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import React from "react";
-import { useAuth } from './context/AuthContext'; // Adjust the path as needed
-import Home from "./components/Home/home";
-import AppHeader from "./components/header/header";
-
-
-import FeedbackForm from "./components/feedback/FeedbackForm";
-import FeedbackList from "./components/feedback/FeedbackList";
-import AdminFeedbackView from "./components/Admin/Feedback/AdminFeedbackView";
-
-import AdminLogin from './components/Admin/AdminLogin/AdminLogin';
-import AdminDashboard from './components/Admin/AdminLogin/AdminDashboard';
-import Chatbot from "./components/Chatbot";
+import './App.css';
+import InventoryHome from "./component/InventoryHome/InventoryHome";
+import InventoryNav from './component/InventoryNav/InventoryNav';
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import LowStockAlerts from "./component/LowStockAlerts/LowStockAlerts";
+import MaintenanceLogs from "./component/MaintenanceLogs/MaintenanceLogs";
+import StockTracking from "./component/StockTracking/StockTracking";
+import UsageReports from "./component/UsageReports/UsageReports";
+import InventoryDetails from "./component/InventoryDetails/InventoryDetails";
+import AddInventory from "./component/AddInventory/AddInventory";
+import UpdateInventory from './component/UpdateInventory/UpdateInventory';
 
 
 function App() {
-  const { isAdminLoggedIn } = useAuth(); // Get the authentication state
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  
   return (
-    <div className="App">
+    <div>
+      <InventoryNav></InventoryNav>
+      <React.Fragment>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/app" element={<AppHeader />} />
-          
-         
-          
-
-          {/* Feedback routes */}
-          <Route path='/feedbackform' element={<FeedbackForm />} />
-          <Route path='/feedbacklist' element={<FeedbackList />} />
-          <Route path='/AdminFeedbackView' element={<AdminFeedbackView />} />
-
-      
-          {/* Admin routes */}
-          <Route path="/adminLogin" element={<AdminLogin />} />
-          <Route
-              path="/adminDashboard"
-              element={isAdminLoggedIn ? <AdminDashboard /> : <Navigate to="/adminLogin" />}
-          />
+        <Route path="/" element={<InventoryDetails/>}/>
+        <Route path="/InventoryHome" element={<InventoryHome/>}/>
+            
+            <Route path="/LowStockAlerts" element={<LowStockAlerts/>}/>
+            <Route path="/MaintenanceLogs" element={<MaintenanceLogs/>}/>
+            <Route path="/StockTracking" element={<StockTracking/>}/>
+            <Route path="/UsageReports" element={<UsageReports/>}/>
+            <Route path="/InventoryDetails" element={<InventoryDetails/>}/>
+            <Route path="/AddInventory" element={<AddInventory/>}/>
+            <Route path="/AddInventory/:InventoryId" element={<UpdateInventory/>}/>
         </Routes>
-
-        {/* Place the ChatBot component here */}
-        <Chatbot/> 
+      </React.Fragment>
     </div>
   );
 }
